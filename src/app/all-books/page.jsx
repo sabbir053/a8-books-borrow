f93bc04c613c5211a6dee73f4ecc90f7ea4c32b0
sabbir053 <sabbirhossain0900@gmail.com>
2026-05-04 19:40:27 +0600

    update api

--- src/app/all-books/page.jsx
+++ src/app/all-books/page.jsx
@@ -1,7 +1,7 @@
 import AllBookPages from '@/components/AllBooks';
 import React from 'react';
 const AllBooksPage = async () => {
-    const res = await fetch('http://localhost:3000/data.json', { cache: 'no-store' });
+    const res = await fetch('https://a8-books-borrow-git-main-sabbir-devs-projects.vercel.app/data.json');
     const booksData = await res.json();
 
     return (
